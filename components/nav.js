bd393f35f793d94dbb29605fcc8b9cafb5d9cb1f
biii-08 <bibishaguragai656@gmail.com>
2023-06-13 12:10:34 -0700

    Initial commit from Create Next App

---
+++ components/nav.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const nav = () => {
+  return (
+    <div className='w-[1921px] h-[148px]  '>
+        <div className=''>
+            
+        </div>
+        
+    </div>
+  )
+}
+
+export default nav
